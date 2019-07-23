4c57398a180ed6232e324eba5b07c278544de8a8
miguelfajardoc <775@holbertonschool.com>
2019-07-23 17:49:13 +0000

    delete and leaf

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,11 +1,31 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_delete -
+ * binary_tree_delete - function that deletes a tree
+ * @tree: the tree to delete
  *
- * Return: 
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-
+	if (tree == NULL)
+		return;
+	while (1)
+	{
+		if (tree->left == NULL && tree->right == NULL)
+		{
+			tree->parent = NULL;
+			free(tree);
+			return;
+		}
+		if (tree->left != NULL)
+		{
+			binary_tree_delete(tree->left);
+			tree->left = NULL;
+		}
+		else
+		{
+			binary_tree_delete(tree->right);
+			tree->right = NULL;
+		}
+	}
 }
--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,11 +1,20 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf -
- *
- * Return: 
+ * binary_tree_is_leaf - check if a node is a leaf
+ * @node: the node to delete
+ * Return: 1 if is a leaf, 0 if not
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-
+	if (node == NULL)
+		return;
+	if (node->parent != NULL)
+	{
+		if (node->left != NULL && node->right != NULL)
+			return (0);
+		else
+			return (1);
+	}
+	return (0);
 }
