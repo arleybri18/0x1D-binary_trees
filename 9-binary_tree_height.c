19e0b53e4b0574b86e98fe0068383b41e0b52f20
miguelfajardoc <775@holbertonschool.com>
2019-07-23 21:05:41 +0000

    [add] postorder height and deepth

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,11 +1,26 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height -
- *
- * Return: 
+ * binary_tree_height - function that count the height of a tree
+ * @tree: the pointer to a tree
+ * Return: the height of tree
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left = 1;
+	size_t right = 1;
 
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left != NULL)
+		left += binary_tree_height(tree->left);
+	if (tree->right != NULL)
+		right += binary_tree_height(tree->right);
+
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (0);
+	}
+	return (MAX(left, right));
 }
