7c83479ac94482e32ffa8e0483cf2326a1872189
miguelfajardoc <775@holbertonschool.com>
2019-07-23 16:51:03 +0000

    [add] function that add a node to the left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,11 +1,22 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left -
- *
- * Return: 
+ * binary_tree_insert_left - create a new node to the left of the parent
+ * @parent: the parent of the node to create
+ * @value: the value of the new node
+ * Return: the new node created
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new_left;
 
+	new_left = binary_tree_node(parent, value);
+	if (parent->left != NULL)
+	{
+		new_left->left = parent->left;
+		new_left->left->parent = new_left;
+	}
+	parent->left = new_left;
+
+	return (new_left);
 }
