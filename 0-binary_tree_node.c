04d529b511b670a39e6067d2973686f54521681a
arleybri18 <arleybri18@gmail.com>
2019-07-23 16:12:30 +0000

    Fixes validate malloc binary tree new node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,20 +1,27 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node -
+ * binary_tree_node - create a nodes for the tree
  *
- * Return: 
+ * @parent: parent of the node
+ * @value: value for the node
+ *
+ * Return: new_node
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
+		/*pointer to new node*/
 		binary_tree_t *new_node;
 
+		/*allocate memory for the new node*/
 		new_node = malloc(sizeof(binary_tree_t));
+		if (new_node == NULL)
+				return NULL;
+		/*assign values*/
 		new_node->parent = parent;
 		new_node->n = value;
 		new_node->left = NULL;
 		new_node->right = NULL;
 
-		return new_node;
-
+		return (new_node);
 }
