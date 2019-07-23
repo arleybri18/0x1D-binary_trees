7b0b169c8faca7021cff1936efe26707146162e6
miguelfajardoc <775@holbertonschool.com>
2019-07-23 17:56:46 +0000

    leaf fix

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -8,10 +8,10 @@
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
-		return;
+		return (0);
 	if (node->parent != NULL)
 	{
-		if (node->left != NULL && node->right != NULL)
+		if (node->left != NULL || node->right != NULL)
 			return (0);
 		else
 			return (1);
