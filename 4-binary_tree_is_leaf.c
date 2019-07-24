5c802d60d0fa5b857c9e3f94eb7dbf6a6b864638
arleybri18 <arleybri18@gmail.com>
2019-07-24 20:26:25 +0000

    [fix] the root is a leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -9,12 +9,10 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
-	if (node->parent != NULL)
-	{
-		if (node->left != NULL || node->right != NULL)
-			return (0);
-		else
-			return (1);
-	}
+
+	if (node->left != NULL || node->right != NULL)
+		return (0);
+	else
+		return (1);
 	return (0);
 }
