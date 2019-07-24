1b6bb9328b18a66ea8e71040dec9fc862bb0feb9
arleybri18 <arleybri18@gmail.com>
2019-07-24 18:43:37 +0000

    [add] find LCA

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,69 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - return the lowest common ancestor of the node
+ *
+ * @first: is a first node to find the LCA
+ * @second: is a second node to find the LCA
+ *
+ * Return: the LCA of the nodes or NULL if not exist
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+{
+	size_t depth_first = 0, depth_second = 0;
+	binary_tree_t *aux1, *aux2;
+
+	/* validate nodes if are NULL*/
+	if (first == NULL || second == NULL)
+			return (NULL);
+	/*validate if they have teh same parent*/
+	if (first->parent == second->parent)
+			return (first->parent);
+	else if (first == second->parent)
+			return (second->parent);
+	else if (second == first->parent)
+			return (first->parent);
+	else
+	{
+		aux1 = first->parent;
+		aux2 = second->parent;
+		/* validate depht of the nodes*/
+		depth_first = binary_tree_depth(aux1);
+		depth_second = binary_tree_depth(aux2);
+		if (depth_first != depth_second)
+		{
+			if(depth_first < depth_second)
+					aux2 = aux2->parent;
+			else
+					aux1 = aux1->parent;
+			if (aux1 == aux2)
+					return (aux1);
+			else
+					binary_trees_ancestor(aux1, aux2);
+		}
+
+	}
+	return (NULL);
+
+}
+
+/**
+ * binary_tree_depth - function that see how deep is a node
+ * @tree: the pointer to the tree
+ * Return: the deep of tree
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t deep = 1;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->parent != NULL)
+	{
+		deep += binary_tree_depth(tree->parent);
+		return (deep);
+	}
+	return (0);
+
+}
