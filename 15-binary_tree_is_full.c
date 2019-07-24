910f9346af546092e75ac6a952e592340accb1cc
arleybri18 <arleybri18@gmail.com>
2019-07-24 21:15:22 +0000

    [fixes] fix validate if the tree is full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,69 +8,18 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	size_t leaves = 0, depth = 0;
-	size_t tree_full = 0;
-	size_t cont = 0;
+	int aux = 0;
 
-	if (tree == NULL)
-		return (0);
-
-	/* return number of leaves*/
-	leaves = binary_tree_leaves(tree);
-	/* return depth of the tree*/
-	depth = binary_tree_depth(tree);
-	/* calculate total leaves full tree*/
-	while (cont < depth)
+	if (tree->left != NULL && tree->right != NULL)
 	{
-		tree_full += 2;
-		cont++;
+		aux = binary_tree_is_full(tree->left);
+		aux = binary_tree_is_full(tree->right);
 	}
-	/* validate if leaves is equal to awaiting total tree*/
-	if (leaves == tree_full)
+	else if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	return (0);
-}
-
-/**
- * binary_tree_leaves - count the number of leaves
- *
- * @tree: node of the tree
- * Return: number of leaves, or 0 otherwise
- */
-size_t binary_tree_leaves(const binary_tree_t *tree)
-{
-	size_t leaves = 0;
-
-	if (tree == NULL)
-		return (0);
-	if (tree->left != NULL)
-		leaves += binary_tree_leaves(tree->left);
-	if (tree->right)
-		leaves += binary_tree_leaves(tree->right);
-	if (tree->left == NULL && tree->right == NULL)
-		leaves++;
-		return (leaves);
-	return (leaves);
-
-}
-
-/**
- * binary_tree_depth - function that see how deep is a node
- * @tree: the pointer to the tree
- * Return: the deep of tree
- */
-size_t binary_tree_depth(const binary_tree_t *tree)
-{
-	size_t deep = 1;
-
-	if (tree == NULL)
+	else
 		return (0);
 
-	if (tree->parent != NULL)
-	{
-		deep += binary_tree_depth(tree->parent);
-		return (deep);
-	}
-	return (0);
+	return (aux);
 
 }
