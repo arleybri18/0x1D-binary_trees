9cd18437d767a2ff3302c545dd8c22538d39c14f
arleybri18 <arleybri18@gmail.com>
2019-07-24 19:50:43 +0000

    [fixes] betty fixes

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,6 +10,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_left;
 
+	if (parent == NULL)
+		return (NULL);
 	new_left = binary_tree_node(parent, value);
 	if (parent->left != NULL)
 	{
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,6 +10,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_right;
 
+	if (parent == NULL)
+		return (NULL);
 	new_right = binary_tree_node(parent, value);
 	if (parent->right != NULL)
 	{
