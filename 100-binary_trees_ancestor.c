c75d55a0a2f78f779aca5aff2e55d24b194e13ad
arleybri18 <arleybri18@gmail.com>
2019-07-24 19:34:36 +0000

    [fix] fixes betty

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -8,43 +8,42 @@
  *
  * Return: the LCA of the nodes or NULL if not exist
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				const binary_tree_t *second)
 {
 	size_t depth_first = 0, depth_second = 0;
 	binary_tree_t *aux1, *aux2;
 
 	/* validate nodes if are NULL*/
 	if (first == NULL || second == NULL)
-			return (NULL);
-	/*validate if they have teh same parent*/
+		return (NULL);
+	/*validate if they have the same parent*/
 	if (first->parent == second->parent)
-			return (first->parent);
-	else if (first == second->parent)
-			return (second->parent);
-	else if (second == first->parent)
-			return (first->parent);
-	else
+		return (first->parent);
+	/* asign auxiliar variables */
+	aux1 = first->parent;
+	aux2 = second->parent;
+	/* validate if the first node is equal to the parent of the second node*/
+	if (first == aux2)
+		return (aux2);
+	/* validate if the second node is equal to the parent of the first node*/
+	else if (second == aux1)
+		return (aux1);
+	/* validate depth of the nodes */
+	depth_first = binary_tree_depth(aux1);
+	depth_second = binary_tree_depth(aux2);
+	/* move the auxiliars and call recursion */
+	if (depth_first != depth_second)
 	{
-		aux1 = first->parent;
-		aux2 = second->parent;
-		/* validate depht of the nodes*/
-		depth_first = binary_tree_depth(aux1);
-		depth_second = binary_tree_depth(aux2);
-		if (depth_first != depth_second)
-		{
-			if(depth_first < depth_second)
-					aux2 = aux2->parent;
-			else
-					aux1 = aux1->parent;
-			if (aux1 == aux2)
-					return (aux1);
-			else
-					binary_trees_ancestor(aux1, aux2);
-		}
-
+		if (depth_first < depth_second)
+			aux2 = aux2->parent;
+		else
+			aux1 = aux1->parent;
+		if (aux1 == aux2)
+			return (aux1);
+		binary_trees_ancestor(aux1, aux2);
 	}
 	return (NULL);
-
 }
 
 /**
