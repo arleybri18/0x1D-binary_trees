dfae51d162eb990b2d5ec32cd4ac23900ecf9136
miguelfajardoc <775@holbertonschool.com>
2019-07-25 23:27:27 +0000

    [add] is complete a binary tree

--- binary_trees.h
+++ binary_trees.h
@@ -43,4 +43,5 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
+int binary_tree_is_complete(const binary_tree_t *tree);
 #endif /* BINARY_TREES_H */
