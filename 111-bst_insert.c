ee065132a5e5369d88979496dcfa47e90f52310f
arleybri18 <arleybri18@gmail.com>
2019-07-26 16:36:15 +0000

    implement bst_insert, not finished

---
+++ 111-bst_insert.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_bst - validate if tree is a Binary Search Tree
+ *
+ * @tree: node of the tree
+ * Return: node sibling or NULL if not exist
+ */
+bst_t *bst_insert(bst_t **tree, int value)
+{
+	bst_t *node;
+	
+	if (*tree == NULL)
+	{
+		node = binary_tree_node(*tree, value);
+		*tree = node;
+	}
+	else
+	{
+		if (value < (*tree)->n)
+			bst_insert(&(*tree)->left, value);
+		else if (value > (*tree)->n)
+			bst_insert(&(*tree)->right, value);
+		else
+			return (NULL);
+		
+	}
+	return (node);
+}
